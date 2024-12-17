43f42443f70163606233bce22862dffb67beae46
emholmgren <150334518+emholmgren@users.noreply.github.com>
2024-12-17 13:55:47 +0100

    Added some media queries for landing and game page.

--- pages/index.js
+++ pages/index.js
@@ -7,26 +7,26 @@ export default function LandingPage() {
     return (
         <div>
             <main className={styles.landingPage}>
-            <Navbar />
-            <div className={styles.heroSection}>
-                <div className={styles.heroText}>
-                <h1>Välkommen till Klä Mig Rätt!</h1>
-                <p>
-                    Lär dig om vädret och hjälp din avatar att klä sig rätt för att möta dagens utmaningar!
-                </p>
-                <div className={styles.ctaButtons}>
-                    <Link href="/weather-facts">
-                    <button className={styles.ctaButton}>Väderfakta</button>
-                    </Link>
-                    <Link href="/game">
-                    <button className={styles.ctaButton}>Börja spela</button>
-                    </Link>
+                <Navbar />
+                <div className={styles.heroSection}>
+                    <div className={styles.heroText}>
+                    <h1>Välkommen till Klä Mig Rätt!</h1>
+                    <p>
+                        Lär dig om vädret och hjälp din avatar att klä sig rätt för att möta dagens utmaningar!
+                    </p>
+                    <div className={styles.ctaButtons}>
+                        <Link href="/weather-facts">
+                        <button className={styles.ctaButton}>Väderfakta</button>
+                        </Link>
+                        <Link href="/game">
+                        <button className={styles.ctaButton}>Börja spela</button>
+                        </Link>
+                    </div>
+                    </div>
+                    <div className={styles.heroImage}>
+                    <img src="/avatars/happy.png" alt="Avatar" />
+                    </div>
                 </div>
-                </div>
-                <div className={styles.heroImage}>
-                <img src="/avatars/happy.png" alt="Avatar" />
-                </div>
-            </div>
             </main>
             <Footer />
         </div>
